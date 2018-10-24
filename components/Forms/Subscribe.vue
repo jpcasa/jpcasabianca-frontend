8590783545275aec2acab0a9e006902b4bcbdacb
Juan Pablo Casabianca <jpc@gcisas.com>
2018-10-23 20:04:32 -0500

    Subscribe

--- components/Forms/Subscribe.vue
+++ components/Forms/Subscribe.vue
@@ -3,9 +3,12 @@
     <p>I hate spam just as much as you...</p>
     <h2>Subscribe to my newsletter</h2>
     <span>I read a lot of stuff every week and love to stay up to date with everything happening. Sometimes it’s overwhelming, so I usually pin out the most important news of the week with a newsletter.</span>
-    <form class="" action="index.html" method="post">
-      <input type="email" name="email" placeholder="Enter your email...">
-      <input type="submit" value="Subscribe">
+    <form action="https://jpasabianca.us19.list-manage.com/subscribe/post?u=36b0406c9ad7cc82decfac1b4&amp;id=8b98119fcd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email..." required>
+      <div style="position: absolute; left: -5000px;" aria-hidden="true">
+        <input type="text" name="b_36b0406c9ad7cc82decfac1b4_8b98119fcd" tabindex="-1" value="">
+      </div>
+      <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
     </form>
   </div>
 </template>
